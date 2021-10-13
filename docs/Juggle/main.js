0df776d5283835f177c7680df2c837944ce1dac1
John <jsanli@ucsc.edu>
2021-10-12 22:31:28 -0700

    initial commit

---
+++ docs/Juggle/main.js
@@ -0,0 +1,41 @@
+title = `
+ Juggle 
+`;
+
+description = `
+
+Click To Juggle 
+`;
+
+characters = [
+	`
+   `
+
+
+];
+
+options = {};
+
+const G = {
+	WIDTH: 300,
+	HEIGHT: 200,
+};
+
+options = {
+	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+    isCapturing: true,
+    isCapturingGameCanvasOnly: true,
+    captureCanvasScale: 2,
+    seed: 1,
+    isPlayingBgm: true,
+    isReplayEnabled: true,
+	isDrawingParticleFront: true,
+    theme: "dark"
+};
+
+
+function update() {
+	if (!ticks) {
+		
+	}
+}
